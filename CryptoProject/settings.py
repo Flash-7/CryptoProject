e8d0330a015b98da1fa2e2cfa6f18927aa6a8814
Dhyey89 <dhyey892@gmail.com>
2023-11-26 22:29:22 -0500

    Delete vercel.json commit

--- CryptoProject/settings.py
+++ CryptoProject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-uswncft)#untzru(!*e%)dx3$b(k2&@$ui+z0ds9r08nzndos*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 WSGI_APPLICATION = 'vercel_app.wsgi.app'
 
 # Application definition
