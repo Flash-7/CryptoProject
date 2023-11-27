2bd118d27e422b788068b25dcaa9476a0510a020
Dhyey89 <dhyey892@gmail.com>
2023-11-26 19:04:29 -0500

    Currency conversion commit

--- cryptoapp/views.py
+++ cryptoapp/views.py
@@ -21,6 +21,7 @@ def create_userprofile(request):
     uprof = UserProfile(user=request.user, balance=0.00)
     uprof.save()
 
+
 def sign_up(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -34,6 +35,7 @@ def sign_up(request):
 
     return render(request, 'registration/signup.html', {"form": form})
 
+
 def user_edit_view(request):
     if request.method == 'POST':
         form = UpdateUserForm(request.POST, instance=request.user.userprofile)
@@ -45,9 +47,38 @@ def user_edit_view(request):
             messages.success(request, 'Profile updated successfully!')
             return redirect('cryptoapp:profile')
     else:
-        form = UpdateUserForm(instance=request.user.userprofile, initial={'first_name':request.user.first_name, 'last_name':request.user.last_name, 'password':request.user.password})
+        form = UpdateUserForm(instance=request.user.userprofile,
+                              initial={'first_name': request.user.first_name, 'last_name': request.user.last_name,
+                                       'password': request.user.password})
 
     return render(request, 'cryptoapp/edit_profile.html', {"form": form})
+
+
+def get_exchange_rates():
+    # For simplicity, providing static exchange rates (not recommended for production)
+    # Replace these values with real exchange rates
+    exchange_rates = {
+        'usd': 1.0,
+        'cad': 1.25,  # Example: 1 USD = 1.25 CAD
+        'gbp': 0.75,  # Example: 1 USD = 0.75 GBP
+        'yen': 110.0,  # Example: 1 USD = 110 JPY
+    }
+    return exchange_rates
+
+
+def convert_currency(original_price, target_currency, exchange_rates):
+    if target_currency in exchange_rates:
+        rate = exchange_rates[target_currency]
+        converted_price = original_price * rate
+        return converted_price
+    else:
+        # Handle the case when the target currency is not found in exchange_rates
+        return original_price
+
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+from .models import Coin
 
 
 def coin_list(request):
@@ -69,7 +100,22 @@ def coin_list(request):
         # If page is out of range (e.g., 9999), deliver last page.
         coins = paginator.page(paginator.num_pages)
 
-    return render(request, 'cryptoapp/coin_list.html', {'coins': coins})
+    # Add currency conversion logic here
+    selected_currency = request.GET.get('currency', 'usd')  # Default to USD if not specified
+    exchange_rates = get_exchange_rates()  # Implement a function to fetch exchange rates
+    currency_symbols = {'usd': '$', 'cad': 'CA$', 'gbp': '£', 'yen': '¥'}
+    for coin in coins:
+        original_price = coin.current_price
+        coin.current_price = convert_currency(original_price, selected_currency, exchange_rates)
+
+        original_market_cap = coin.market_cap
+        coin.market_cap = convert_currency(original_market_cap, selected_currency, exchange_rates)
+
+        original_total_volume = coin.total_volume
+        coin.total_volume = convert_currency(original_total_volume, selected_currency, exchange_rates)
+
+    return render(request, 'cryptoapp/coin_list.html',
+                  {'coins': coins, 'selected_currency': selected_currency, 'currency_symbols': currency_symbols})
 
 
 def coin_details(request, id):
@@ -208,7 +254,7 @@ def portfolio(request):
     user = request.user
     user_profile = UserProfile.objects.get(user=user)
     user_portfolio = user_profile.portfolio_set.all()
-    
+
     for i in user_portfolio:
         print(i, i.user_profile.user, i.coin, i.quantity, i.amount)
 
@@ -227,7 +273,7 @@ def portfolio(request):
             return redirect('cryptoapp:portfolio')
 
     return render(request, 'cryptoapp/portfolio.html',
-                  {'assets': user_portfolio,'add_balance_form': add_balance_form, 'user_profile': user_profile})
+                  {'assets': user_portfolio, 'add_balance_form': add_balance_form, 'user_profile': user_profile})
 
 
 def toggle_watchlist(request, coin_id):
@@ -242,6 +288,7 @@ def toggle_watchlist(request, coin_id):
         in_watchlist = True
 
     return JsonResponse({'in_watchlist': in_watchlist})
+
 
 def watchlist(request):
     user = request.user
