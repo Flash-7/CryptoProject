d6929c21a44a5e76efeb53d5bc0a800e61e841dd
Dhyey89 <dhyey892@gmail.com>
2023-11-27 17:44:56 -0500

    Hosting commit

--- CryptoProject/settings.py
+++ CryptoProject/settings.py
@@ -50,12 +50,12 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
-# EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'devcryptoproject@gmail.com'
+EMAIL_HOST_PASSWORD = 'gelhxjazfmpinusu'
+EMAIL_PORT =587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 
 
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'CryptoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"cryptoapp/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
